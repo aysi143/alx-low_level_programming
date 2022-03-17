de2cea084329a7c0227c87ab9d713163f498664c
AYSI <aysanewyonasj@gmail.com>
2022-03-17 21:15:36 +0300

    commit on 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - returns the product of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# More functions and tested loops
+
