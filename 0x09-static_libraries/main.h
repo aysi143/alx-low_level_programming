6646fe99926790e05c00a6195148c59ce557309f
AYSI <aysanewyonasj@gmail.com>
2022-04-03 09:23:43 +0300

    main.h modified

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -21,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /*MAIN.H OYUGI BRUCE 2017*/
+#endif
