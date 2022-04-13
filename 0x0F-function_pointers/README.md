1bd8caa3685950f17cf4c8c9b916f28801c9f188
AYSI <aysanewyonasj@gmail.com>
2022-04-13 11:05:46 +0300

    betty style | 100-main_opcodes.c README.md function_pointers.h

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# This is README.md file for the project of 0x0F function_pointers 
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
