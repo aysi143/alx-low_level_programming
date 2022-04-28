fdf00b2168e8c17f880240dc7141625d465f2cf1
AYSI <aysanewyonasj@gmail.com>
2022-04-28 17:28:04 +0300

    checker refusing-solved | 0-binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
+
+	if (b == '\0')
+		return (0);
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
