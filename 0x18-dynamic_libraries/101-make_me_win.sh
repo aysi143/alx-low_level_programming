4fdd7bcb07ce9c854d9ac913c4a10831ccaf3d64
AYSI <aysanewyonasj@gmail.com>
2022-06-13 22:18:03 +0300

    hike a

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 gcc *.o -fPIC -shared -o libruin.so
-<<<<<<< HEAD
-LD_PRELOAD=$PWD/libruin.so
-=======
-LD_PRELOAD=$PWD/libruin.so
->>>>>>> 91e6dbaa210931d3b208b27e811a1ff23e1c006b
+LD_PRELOAD=$WPD/libruin.so
