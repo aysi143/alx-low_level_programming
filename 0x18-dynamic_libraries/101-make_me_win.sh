caf3a599c96a76d40d5172620d414dea5bd0941c
Aysanew Yonas <aysanewyonasj@gmail.com>
2022-06-13 21:39:21 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aysi143/alx-low_level_programming/edit/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/aysi143/alx-low_level_programming/edit/master/0x18-dynamic_libraries/ic1.so
 export LD_PRELOAD=/tmp/libmask.so
