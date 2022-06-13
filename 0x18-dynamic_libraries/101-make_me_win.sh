91e6dbaa210931d3b208b27e811a1ff23e1c006b
Aysanew Yonas <aysanewyonasj@gmail.com>
2022-06-13 21:57:26 +0300

    have you tired of bing shits?

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/aysi143/alx-low_level_programming/edit/master/0x18-dynamic_libraries/ic1.so
-export LD_PRELOAD=/tmp/ic1.so
+gcc *.o -fPIC -shared -o libruin.so
+LD_PRELOAD=$PWD/libruin.so
